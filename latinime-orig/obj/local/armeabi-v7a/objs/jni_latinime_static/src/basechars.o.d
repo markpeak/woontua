f76f88a62002d3374c91b568e8b16b9e49036453
Isriya Paireepairit <markpeak@gmail.com>
2012-10-14 16:49:30 +0700

    fix directory name

--- latinime-orig/obj/local/armeabi-v7a/objs/jni_latinime_static/src/basechars.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/armeabi-v7a/objs/jni_latinime_static/src/basechars.o: \
- jni/src/basechars.cpp jni/src/char_utils.h
-
-jni/src/char_utils.h:
--- latinime-orig/obj/local/x86/objs/jni_latinime_static/src/basechars.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/x86/objs/jni_latinime_static/src/basechars.o: \
- jni/src/basechars.cpp jni/src/char_utils.h
-
-jni/src/char_utils.h:
